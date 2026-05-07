895d6f766050ddc9401d8068293d4d93c6827aee
Anderson <andersonfrfilho@gmail.com>
2026-05-07 12:08:58 -0300

    Revert "fix: add comprehensive RabbitMQ connection lifecycle logging to CRON backend"
    This reverts commit 45ef24a73d28b1ad3e2f49f34f6f229bc46d3acb.

--- src/modules/shared/providers/queue/producer/implementations/rabbitmq/rabbit.module.ts
+++ src/modules/shared/providers/queue/producer/implementations/rabbitmq/rabbit.module.ts
@@ -4,14 +4,12 @@ import { Module } from '@nestjs/common';
 import { QUEUE_PRODUCER_PROVIDER } from '../../producer.token';
 import { CronRabbitMQModule } from '@modules/shared/rabbitmq/rabbitmq.module';
 
-import { RabbitConnectionLifecycleListener } from './rabbit.connection-lifecycle';
 import { RabbitBindingsService } from './rabbit.bindings.service';
 import { RabbitMQMessageProducer } from './rabbit.provider';
 
 @Module({
   imports: [LoggerModule, CronRabbitMQModule],
   providers: [
-    RabbitConnectionLifecycleListener,
     {
       provide: QUEUE_PRODUCER_PROVIDER,
       useClass: RabbitMQMessageProducer,
