b19aad269fbd87ac8e1f347d972be7b2c0798bf7
Anderson <andersonfrfilho@gmail.com>
2026-05-23 10:00:17 -0300

    fix: add MetricsModule import to AccountCleanupModule to resolve CronMetricsService dependency

--- src/modules/account-cleanup/account-cleanup.module.ts
+++ src/modules/account-cleanup/account-cleanup.module.ts
@@ -3,12 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { User } from '@modules/shared/providers/database/entities/user.entity';
 import { CONNECTIONS_NAMES } from '@modules/shared/providers/database/database.constant';
+import { MetricsModule } from '@modules/metrics/metrics.module';
 
 import { AccountCleanupService } from './account-cleanup.service';
 import { AccountCleanupJob } from './account-cleanup.job';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User], CONNECTIONS_NAMES.POSTGRES)],
+  imports: [TypeOrmModule.forFeature([User], CONNECTIONS_NAMES.POSTGRES), MetricsModule],
   providers: [AccountCleanupService, AccountCleanupJob],
   exports: [AccountCleanupService],
 })
